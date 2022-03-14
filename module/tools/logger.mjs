7c3528b948f99d4a93c9955c11d8c70caf3e57e6
Joe Martino <taragnor@yahoo.com>
2022-03-14 01:26:49 -0400

    Fix for the CoM Logger (now displays token alias)

--- module/tools/logger.mjs
+++ module/tools/logger.mjs
@@ -6,7 +6,8 @@ export class Logger {
 
 	static async gmMessage(text, actor = null) {
 		const gmIds = game.users.filter( x=> x.role == CONST.USER_ROLES.GAMEMASTER);
-		const speaker = ChatMessage.getSpeaker({actor});
+		// const speaker = ChatMessage.getSpeaker({actor});
+		const speaker = ChatMessage.getSpeaker({alias: actor.getDisplayedName()});
 		let messageData = {
 			speaker: speaker,
 			content: text,
@@ -17,7 +18,8 @@ export class Logger {
 	}
 
 	static async sendToChat(text, sender={}) {
-		const speaker = ChatMessage.getSpeaker(sender);
+		// const speaker = ChatMessage.getSpeaker(sender);
+		const speaker = ChatMessage.getSpeaker({alias: actor.getDisplayedName()});
 		let messageData = {
 			speaker: speaker,
 			content: text,
