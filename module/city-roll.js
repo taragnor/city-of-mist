65ea4b55bf3c37f2ba5258e9b9daec628add8893
Joe Martino <taragnor@yahoo.com>
2022-05-04 23:04:39 -0400

    penalty scaling update

--- module/city-roll.js
+++ module/city-roll.js
@@ -323,10 +323,10 @@ export class CityRoll {
 			switch (effectBonus) {
 				case 0: return 0;
 				case 1: return -1;
-				case 2: return -2;
-				case 3: return -4;
-				case 4: return -6;
-				case 5: return -8;
+				case 2: return -3;
+				case 3: return -5;
+				case 4: return -7;
+				case 5: return -9;
 				default: return 0;
 			}
 		}
