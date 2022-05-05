6d48ff5ae0b26fe047bed8d650be6d95e9225994
Joe Martino <taragnor@yahoo.com>
2022-05-04 22:34:13 -0400

    dead code removal

--- module/city-roll.js
+++ module/city-roll.js
@@ -169,13 +169,6 @@ export class CityRoll {
 				strikeout: false,
 			});
 		}
-		//NOTE: bug was related to deleted tags showing up. It should be fixed with filter statement above
-		const usedWeaknessTag = tags.some( x=> {
-			const tag = CityHelpers.getOwner(x.ownerId).getTag(x.tagId);
-			return x.type == "tag"
-				&& tag.data.data.subtype == "weakness"
-				&& x.amount < 0
-		});
 		this.#modifiers = modifiers;
 		this.#options.modifiers = modifiers;
 		this.#tags = tags;
