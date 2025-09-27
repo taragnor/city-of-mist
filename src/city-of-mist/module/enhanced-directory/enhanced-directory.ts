6d12250aa959bdeffcc8884367eadfeff39bf166
Joe Martino <taragnor@yahoo.com>
2025-09-26 23:27:52 -0400

    bugfix: enhanced directory search (really hacky code)

--- src/city-of-mist/module/enhanced-directory/enhanced-directory.ts
+++ src/city-of-mist/module/enhanced-directory/enhanced-directory.ts
@@ -99,73 +99,130 @@ export class EnhancedActorDirectory {
 		/// @ts-ignore
 		ActorDirectory._entryPartial =  "systems/city-of-mist/module/enhanced-directory/enhanced-template.hbs";
 
-  ActorDirectory.prototype._onSearchFilter = function (this: ActorDirectory, _event: Event, query: unknown, rgx: RegExp, html: HTMLElement) {
-    const isSearch = !!query;
-    const documentIds = new Set();
-    const folderIds = new Set();
-    const autoExpandFolderIds = new Set();
+		//@ts-ignore
+		ActorDirectory.prototype._matchSearchEntries = function(query : string, entryIds :string[], folderIds: string[], autoExpandIds: boolean, options={}) {
+			//@ts-ignore
+    const nameOnlySearch = this.collection.searchMode === CONST.DIRECTORY_SEARCH_MODES.NAME;
+    const entries = this.collection.index ?? this.collection.contents;
 
-    // Match documents and folders
-    if ( isSearch ) {
+    // Copy the folderIds to a new set, so that we can add to the original set without incorrectly adding child entries.
+    const matchedFolderIds = new Set(folderIds);
 
-      // Include folders and their parents
-      function includeFolder(folder: Folder, autoExpand=true) {
-        if ( !folder ) return;
-        if ( folderIds.has(folder.id) ) return;
-        folderIds.add(folder.id);
-        if ( autoExpand ) autoExpandFolderIds.add(folder.id);
-        if ( folder.folder ) includeFolder(folder.folder); // Always autoexpand parent folders
+    for ( const entry of entries ) {
+      const entryId = entry._id;
+
+      // If we matched a folder, add its child entries.
+		 //@ts-ignore
+      if ( matchedFolderIds.has(entry.folder?._id ?? entry.folder) ) entryIds.add(entryId);
+
+      // Otherwise, if we are searching by name, match the entry name.
+		 //@ts-ignore
+      if ( nameOnlySearch && query.test(foundry.applications.ux.SearchFilter.cleanQuery(entry.directoryName)) ) {
+		 //@ts-ignore
+        entryIds.add(entryId);
+		 //@ts-ignore
+        this.onMatchFolder(entry.folder, folderIds, autoExpandIds);
       }
+    }
 
-		 // Match documents by name
-		 for ( let d of this.documents ) {
-			 const searchName :string = "directoryName" in d ? d.directoryName as string: d.name;
-			 if ( rgx.test(SearchFilter.cleanQuery(searchName)) ) {
-				 documentIds.add(d.id);
-				 //@ts-expect-error
-				 includeFolder(d.folder);
-			 }
-		 }
+    if ( nameOnlySearch ) return;
 
-		 // Match folders by name
-		 for ( let f of this.folders ) {
-			 //@ts-ignore
-			 if ( rgx.test(SearchFilter.cleanQuery(f?.directoryName ?? f.name)) ) {
-				 includeFolder(f, false);
-				 for ( let d of this.documents.filter(x => x.folder === f) ) {
-					 documentIds.add(d.id);
-				 }
-			 }
-		 }
-	 }
-
-    // Toggle each directory item
-    for ( let el of html.querySelectorAll<HTMLElement>(".directory-item") ) {
-
-		 // Documents
-		 if (el.classList.contains("document")) {
-			 el.style.display = (!isSearch || documentIds.has(el.dataset.documentId)) ? "flex" : "none";
-		 }
-
-		 // Folders
-		 if (el.classList.contains("folder")) {
-			 let match = isSearch && folderIds.has(el.dataset.folderId);
-			 el.style.display = (!isSearch || match) ? "flex" : "none";
-			 if ( autoExpandFolderIds.has(el.dataset.folderId) ) {
-				 if ( isSearch && match ) el.classList.remove("collapsed");
-			 //@ts-ignore
-				 else el.classList.toggle("collapsed", !game.folders._expanded[el.dataset.folderId]);
-			 }
-		 }
-	 }
+    // Full text search.
+		 //@ts-ignore
+    const matches = this.collection.search({ query: query.source, exclude: Array.from(entryIds) });
+    for ( const match of matches ) {
+		 //@ts-ignore
+      if ( entryIds.has(match._id) ) continue;
+		 //@ts-ignore
+      entryIds.add(match._id);
+		 //@ts-ignore
+        this.onMatchFolder(entry.folder, folderIds, autoExpandIds);
+    }
   }
 
+		//@ts-ignore
+		ActorDirectory.prototype.onMatchFolder= function(folder: Folder, folderIds: string[], autoExpandIds: boolean, { autoExpand=true }={}) {
+			//@ts-ignore
+			if ( typeof folder === "string" ) folder = game.packs.folders.get(folder);
+			if ( !folder ) return;
+			//@ts-ignore
+			const folderId = folder._id;
+			//@ts-ignore
+			const visited = folderIds.has(folderId);
+			//@ts-ignore
+			folderIds.add(folderId);
+			//@ts-ignore
+			if ( autoExpand ) autoExpandIds.add(folderId);
+			//@ts-ignore
+			if ( !visited && folder.folder ) this.onMatchFolder(folder.folder, folderIds, autoExpandIds);
+		}
+
+  //ActorDirectory.prototype._onSearchFilter = function (this: ActorDirectory, _event: Event, query: unknown, rgx: RegExp, html: HTMLElement) {
+  //  const isSearch = !!query;
+  //  const documentIds = new Set();
+  //  const folderIds = new Set();
+  //  const autoExpandFolderIds = new Set();
+
+  //  // Match documents and folders
+  //  if ( isSearch ) {
+
+  //    // Include folders and their parents
+  //    function includeFolder(folder: Folder, autoExpand=true) {
+  //      if ( !folder ) return;
+  //      if ( folderIds.has(folder.id) ) return;
+  //      folderIds.add(folder.id);
+  //      if ( autoExpand ) autoExpandFolderIds.add(folder.id);
+  //      if ( folder.folder ) includeFolder(folder.folder); // Always autoexpand parent folders
+  //    }
+
+		 //// Match documents by name
+		 //for ( let d of this.documents ) {
+			 //const searchName :string = "directoryName" in d ? d.directoryName as string: d.name;
+			 //if ( rgx.test(SearchFilter.cleanQuery(searchName)) ) {
+				 //documentIds.add(d.id);
+				 ////@ts-expect-error
+				 //includeFolder(d.folder);
+			 //}
+		 //}
+
+		 //// Match folders by name
+		 //for ( let f of this.folders ) {
+			 ////@ts-ignore
+			 //if ( rgx.test(SearchFilter.cleanQuery(f?.directoryName ?? f.name)) ) {
+				 //includeFolder(f, false);
+				 //for ( let d of this.documents.filter(x => x.folder === f) ) {
+					 //documentIds.add(d.id);
+				 //}
+			 //}
+		 //}
+	 //}
+
+  //  // Toggle each directory item
+  //  for ( let el of html.querySelectorAll<HTMLElement>(".directory-item") ) {
+
+		 //// Documents
+		 //if (el.classList.contains("document")) {
+			 //el.style.display = (!isSearch || documentIds.has(el.dataset.documentId)) ? "flex" : "none";
+		 //}
+
+		 //// Folders
+		 //if (el.classList.contains("folder")) {
+			 //let match = isSearch && folderIds.has(el.dataset.folderId);
+			 //el.style.display = (!isSearch || match) ? "flex" : "none";
+			 //if ( autoExpandFolderIds.has(el.dataset.folderId) ) {
+				 //if ( isSearch && match ) el.classList.remove("collapsed");
+			 ////@ts-ignore
+				 //else el.classList.toggle("collapsed", !game.folders._expanded[el.dataset.folderId]);
+			 //}
+		 //}
+	 //}
+  //}
+
 		ActorDirectory._sortAlphabetical = function (a: CityActor, b: CityActor) {
-			if (a?.directoryName)
+			if (a?.directoryName && b?.directoryName)
 				return a.directoryName.localeCompare(b.directoryName);
 			else return a.name.localeCompare(b.name);
 		}
-		// ui.actors.render(true);
 
 		console.log("Enhanced directory applied");
 	}
